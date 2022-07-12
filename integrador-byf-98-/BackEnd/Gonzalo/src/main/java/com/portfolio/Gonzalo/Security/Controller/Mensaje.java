32623602576bb9ec4968b2cc7877224e8f2e842f
gonza7795 <gonza_7795@yahoo.com.ar>
2022-07-11 21:09:42 -0300

    hasta 1.49.31

---
+++ integrador-byf-98-/BackEnd/Gonzalo/src/main/java/com/portfolio/Gonzalo/Security/Controller/Mensaje.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.portfolio.Gonzalo.Security.Controller;
+
+/**
+ *
+ * @author Gonza
+ */
+public class Mensaje {
+    
+    private String mensaje;
+    
+    //constructores
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    //getter and setter
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+    
+    
+}
